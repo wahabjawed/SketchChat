02f9e58d576603a8aa430219aaa6ab7090319523
Wahab Jawed <wahabjawed@gmail.com>
2014-10-17 23:20:36 +0500

    commit

---
+++ web service/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "shareit"); // db user
+define('DB_PASSWORD', "infinity1"); // db password (mention your db password here)
+define('DB_DATABASE', "shareit"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
