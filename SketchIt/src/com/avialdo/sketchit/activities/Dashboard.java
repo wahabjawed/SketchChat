9959bf8e2f48ffa88bf6f2958d2580cdbbad0433
Wahab Jawed <wahabjawed@gmail.com>
2014-10-24 00:20:49 +0500

    Canvas implemented

--- SketchIt/src/com/avialdo/sketchit/activities/Dashboard.java
+++ SketchIt/src/com/avialdo/sketchit/activities/Dashboard.java
@@ -25,7 +25,7 @@ public class Dashboard extends ActionBarActivity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.tabs);
 
 		// Initilization
 		viewPager = (ViewPager) findViewById(R.id.pager);
--- SketchIt/src/com/avialdo/sketchit/fragments/TopRatedFragment.java
+++ SketchIt/src/com/avialdo/sketchit/fragments/TopRatedFragment.java
@@ -1,14 +1,18 @@
 package com.avialdo.sketchit.fragments;
 
-import com.avialdo.sketchit.R;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class TopRatedFragment extends Fragment {
+import com.avialdo.sketchit.R;
+import com.avialdo.sketchit.abstracts.SketchFragment;
+import com.avialdo.sketchit.activities.Canvas;
+
+public class TopRatedFragment extends SketchFragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,7 +20,29 @@ public class TopRatedFragment extends Fragment {
 
 		View rootView = inflater.inflate(R.layout.fragment_top_rated,
 				container, false);
+		Button b = (Button) rootView.findViewById(R.id.button1);
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(TopRatedFragment.this.getActivity(),
+						Canvas.class));
+			}
+		});
 
 		return rootView;
+	}
+
+	@Override
+	public void setupView(View layoutView) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setupListner() {
+		// TODO Auto-generated method stub
+
 	}
 }
